80824fdbc3103d7689cf8ee859d8511507382d7f
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-09-09 11:19:59 +0200

    Make all controller tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+	# Static Pages
+	root 'static_pages#home'
 	get 'events/new'
   get 'events/create'
   get 'events/show'
   get 'events/index'
+	
+	# Users
 	resources :users
+	
+	# Sessions
+	resources :events
 	get  '/login', to: 'sessions#new'
 	post '/login', to: 'sessions#create'
-	resources :events
+
 end
